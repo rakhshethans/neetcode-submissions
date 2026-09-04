1a077e353d404f2b3d3f800714b49c7fcee10332
rakhshethans <rakhshethans@gmail.com>
2026-09-04 19:40:35 +0100

    Add: longest-repeating-substring-with-replacement - submission-6

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-6.py
@@ -0,0 +1,53 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # use a frequency dictionary to track the frequency of characters
+        # the most frequent character should be the repeating character
+        # if the total num of other characters is k, then fine
+
+        frequencyDict = dict()
+
+        # use a variable sliding window
+        # it grows while the number of non-majority characters in frequencyDict <= k or current char is a majority
+        # it shrinks if the num of non-majority characters in frequencyDict > k, until this is true
+
+        left = 0
+        right = 0
+        maxLength = 0
+        maxFreq = 0
+        maxFreqChar = ""
+
+        while right < len(s):
+
+            #.get(), if there is no s[right] key, then it will return 0
+            # useful if its the first time we are seeing a character
+            frequencyDict[s[right]] = frequencyDict.get(s[right], 0) + 1
+            
+            # check if it is the max character now
+            maxFreq = max(maxFreq, frequencyDict[s[right]])
+
+            length = right - left + 1
+            # if the number of non majority chars is now greater than k, the substring is invalid so we must shrink
+            while length - maxFreq > k:
+                frequencyDict[s[left]] -= 1
+                left += 1
+                length -= 1
+            
+            maxLength = max(length, maxLength)
+
+            right += 1  
+        
+        return maxLength
+
+
+
+        ### why don't we decrease maxFreq when removing a majority char
+        # we use maxFreq monotonically instead of actual max frequency, we only update it when there is a new char that has a greater frequency of chars
+        # this means that when we shrink, we don't always end up with a valid window
+        # but that doesn't matter because we have already found a window that is the same size or larger and is definitely valid when that maxFreq was genuinely present
+        # we update length continuously
+
+
+                        
+                        
+
+                
